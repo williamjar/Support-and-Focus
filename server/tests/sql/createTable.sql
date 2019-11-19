126959df768747b5c0dbcb6a63efc89ed8fa1bdd
William <williaj@stud.ntnu.no>
2019-11-19 20:46:28 +0100

    Started integrating tests.

--- server/tests/sql/createTable.sql
+++ server/tests/sql/createTable.sql
@@ -1,4 +1,3 @@
-drop table if exists comments;
 drop table  if exists article;
 
 CREATE TABLE ticket(
@@ -12,26 +11,4 @@ CREATE TABLE ticket(
                        group_id int,
                        author VARCHAR(255),
                        PRIMARY KEY(ticket_id)
-)
-
-CREATE TABLE comment(
-                        comment_id INT NOT NULL AUTO_INCREMENT,
-                        content VARCHAR(255),
-                        priority int DEFAULT 1,
-                        post_date DATETIME,
-                        ticket_id int,
-                        PRIMARY KEY (comment_id)
-)
-
-CREATE TABLE archive(
-                       ticket_id int,
-                       headline VARCHAR(255),
-                       content VARCHAR(255),
-                       priority int DEFAULT 2,
-                       picture VARCHAR(255),
-                       post_date DATETIME,
-                       email VARCHAR(255),
-                       group_id int,
-                       author VARCHAR(255),
-                       PRIMARY KEY(ticket_id)
-)
+);
