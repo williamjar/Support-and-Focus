ae6e0deb1c9c40328accdd26b966b2e8734c6ffc
William <williaj@stud.ntnu.no>
2019-11-19 20:23:23 +0100

    Started integrating tests.

--- server/tests/sql/create_testdata.sql
+++ server/tests/sql/create_testdata.sql
@@ -1,1 +1,1 @@
-INSERT INTO article ('1000', 'Jeg trenger hjelp', '1', 'picture.png', '2019-03-15 01:01:22','william@jarbeaux.no',1, 'William Jarbeaux');
+INSERT INTO ticket('1000', 'Jeg trenger hjelp', '1', 'picture.png', '2019-03-15 01:01:22','william@jarbeaux.no',1, 'William Jarbeaux');
