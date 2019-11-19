feb6b9e5f7d4d53af72403394bf7e74a9c41c127
William <williaj@stud.ntnu.no>
2019-11-19 20:39:28 +0100

    Started integrating tests.

--- server/tests/ticketdao.js
+++ server/tests/ticketdao.js
@@ -2,7 +2,7 @@ const Dao = require("./dao.js");
 
 module.exports = class Articledao extends Dao {
     getAllTickets(callback) {
-        super.query("select article_id, headline, content, priority, picture, post_date, category, author from article where visible=1 limit 20", [], callback);
+        super.query("SELECT * FROM ticket", [], callback);
     }
 
 }
