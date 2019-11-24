ed7e2050e5c1ee5ffa0465f1d6d4b6d5909f5484
William <williaj@stud.ntnu.no>
2019-11-24 19:41:42 +0100

    client tests

---
+++ client/react-api/build/precache-manifest.1eddf0727dfaa19c388fd301dfb2160d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc0c40d640b0703cd2871ee1d99894ed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9abbf7f317cdf406b964",
+    "url": "/static/js/2.8f8654bf.chunk.js"
+  },
+  {
+    "revision": "08ea996c4c1467755f67",
+    "url": "/static/js/main.49f2cd86.chunk.js"
+  },
+  {
+    "revision": "bbd0a38a4cb99fe1d64a",
+    "url": "/static/js/runtime-main.b6c99bc5.js"
+  }
+]);
